c9042e4ea06cd9bbccf686cf8afbc9eb38db9fa3
Vyacheslav K <vyacheslaw.kilin@mail.ru>
2023-01-07 23:17:28 +0300

    Правка блока Обо мне, добавил грид сетку и новые изображения. Нужно скорректировать размер!!!!

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,6 +4,7 @@ import './AboutMe.css';
 import aboutMeOne from '../../images/about/aboutme1.png';
 import aboutMeTwo from '../../images/about/aboutme2.png';
 import aboutMeThree from '../../images/about/aboutme3.png';
+import aboutMeFour from '../../images/about/aboutme4.png';
 const AboutMe = () => {
     const location = useLocation();
     const abouMeRef = useRef(null);
@@ -17,26 +18,19 @@ const AboutMe = () => {
     return (
         <div ref={abouMeRef} id="about_me" className='AboutMe'>
             <div className='container'>
-                <div className='about-me__left-circle'></div>
-                <div className='about-me__right-top'></div>
-                <div className='about-me__right-bottom'></div>
+                <div className='about_me-title'>
+                <h1>Обо мне</h1>
+                </div>
+                <div className='about_me--container'>
+                    <div className='about-me-item about-me1'><img src={aboutMeOne} alt="Мое фото" /></div>
+                    <div className='about-me-item about-me2'>Еще раз всем привет! Итак, вы уже знаете, что меня зовут Слава. Немного о себе: 30 лет, работаю в процессиональной Веб-Студии. Почему программирование? Все элементарно - мне это нравится, профессия будущего, благодаря которой я могу обеспечить себя и осуществить свою мечту - путешествовать, на данный момент я специализируюсь на веб-дизайне, front-end и back-end разработке, сайтах под ключ. Почему вы должны выбрать меня? Я подхожу к каждому заказу с большой ответственностью и любовью, так как хочу сделать себе имя, исключить плагиат и халатность, полностью изучить проект, клиента и его целевую аудиторию, работать на качество, стараясь сделать заказ максимально быстро и уникально, учитывая все правки и желания.
+                        Доверившись мне, вы получите максимальную отдачу от своего проекта, сэкономите свои нервы и время.
+                        Если я вас заинтересовал, вы хотите узнать что-то еще или воспользоваться моими услугами, то я предоставлю все свои контакты в нижней части страницы или во выкладке "Контакты"</div>
+                    <div className='about-me-item about-me3'><img src={aboutMeTwo} alt="Мое фото" /></div>
+                    <div className='about-me-item about-me4'><img src={aboutMeThree} alt="Мое фото" /></div>
+                    <div className='about-me-item about-me5'><img src={aboutMeFour} alt="Мое фото" /></div>
+                </div>
 
-                <div className='about-me__container__title'>ОБО МНЕ
-                </div>
-                <div className='about-me__photo-left-top'>
-                    <img src={aboutMeOne} alt="Мое фото" />
-                </div>
-                <div className='about-me__photo-left-bottom'>
-                    <img src={aboutMeTwo} alt="Мое фото" />
-                </div>
-                <div className='about-me__center-card'>
-                    Еще раз всем привет! Итак, вы уже знаете, что меня зовут Слава. Немного о себе: 30 лет, работаю в процессиональной Веб-Студии. Почему программирование? Все элементарно - мне это нравится, профессия будущего, благодаря которой я могу обеспечить себя и осуществить свою мечту - путешествовать, на данный момент я специализируюсь на веб-дизайне, front-end и back-end разработке, сайтах под ключ. Почему вы должны выбрать меня? Я подхожу к каждому заказу с большой ответственностью и любовью, так как хочу сделать себе имя, исключить плагиат и халатность, полностью изучить проект, клиента и его целевую аудиторию, работать на качество, стараясь сделать заказ максимально быстро и уникально, учитывая все правки и желания.
-                    Доверившись мне, вы получите максимальную отдачу от своего проекта, сэкономите свои нервы и время.
-                    Если я вас заинтересовал, вы хотите узнать что-то еще или воспользоваться моими услугами, то я предоставлю все свои контакты ниже.
-                </div>
-                <div className='about-me__photo-right'>
-                    <img src={aboutMeThree} alt="Мое фото" />
-                </div>
             </div>
         </div>
     )
