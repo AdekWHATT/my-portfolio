1c7ec3e6851d8657dd9bc3b020ec2ff0119f4d52
Vyacheslav K <vyacheslaw.kilin@mail.ru>
2023-02-09 17:32:02 +0300

    добавил скриншоты сайтов для портфолио

--- src/components/Sites/Sites.js
+++ src/components/Sites/Sites.js
@@ -3,6 +3,15 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Sites.css';
+import avtoelectrika from '../../images/portfolio/avtoelectrika.jpg';
+import bavlymuka from '../../images/portfolio/bavly-muka.jpg';
+import frbtlt from '../../images/portfolio/frbtlt.jpg';
+import gippokrat16 from '../../images/portfolio/gippokrat16.jpg';
+import kafolat from '../../images/portfolio/kafolat.jpg';
+import ngta from '../../images/portfolio/ngta.jpg';
+import prodservis from '../../images/portfolio/prodservis.jpg';
+import upakovka from '../../images/portfolio/upakovka.jpg';
+
 const Sites = () => {
     const settings = {
         dots: false,
@@ -18,13 +27,28 @@ const Sites = () => {
                 <div>
                     <Slider {...settings}>
                         <div>
-                            <h3>1</h3>
+                            <img src={avtoelectrika} alt="Автоэлектрика" />
                         </div>
                         <div>
-                            <h3>1</h3>
+                            <img src={bavlymuka} alt="Бавлы Мука" />
                         </div>
                         <div>
-                            <h3>1</h3>
+                            <img src={frbtlt} alt="Школа Карате" />
+                        </div>
+                        <div>
+                            <img src={gippokrat16} alt="Гиппократ" />
+                        </div>
+                        <div>
+                            <img src={kafolat} alt="Кафолат" />
+                        </div>
+                        <div>
+                            <img src={ngta} alt="НГТА" />
+                        </div>
+                        <div>
+                            <img src={prodservis} alt="НГТА" />
+                        </div>
+                        <div>
+                            <img src={upakovka} alt="Упаковка" />
                         </div>
                     </Slider>
                 </div>
@@ -33,4 +57,4 @@ const Sites = () => {
     )
 }
 
-export default Sites
+export default Sites;
