a06efb78d744d2c859c2e3b282381321b90aa6a6
Vyacheslaw Kilin <vyacheslaw.kilin@mail.ru>
2023-02-12 17:15:04 +0300

    заменил портфолио на грид сетку, немного адаптива

--- src/components/Sites/Sites.js
+++ src/components/Sites/Sites.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import './Sites.css';
 import avtoelectrika from '../../images/portfolio/avtoelectrika.jpg';
 import bavlymuka from '../../images/portfolio/bavly-muka.jpg';
@@ -13,44 +10,43 @@ import prodservis from '../../images/portfolio/prodservis.jpg';
 import upakovka from '../../images/portfolio/upakovka.jpg';
 
 const Sites = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
     return (
         <div className='Sites'>
             <div className='container'>
-                <h1 className='sites-title'>Мое Портфолио</h1>
-                <div>
-                    <Slider {...settings}>
-                        <div>
-                            <img src={avtoelectrika} alt="Автоэлектрика" />
-                        </div>
-                        <div>
-                            <img src={bavlymuka} alt="Бавлы Мука" />
-                        </div>
-                        <div>
-                            <img src={frbtlt} alt="Школа Карате" />
-                        </div>
-                        <div>
-                            <img src={gippokrat16} alt="Гиппократ" />
-                        </div>
-                        <div>
-                            <img src={kafolat} alt="Кафолат" />
-                        </div>
-                        <div>
-                            <img src={ngta} alt="НГТА" />
-                        </div>
-                        <div>
-                            <img src={prodservis} alt="НГТА" />
-                        </div>
-                        <div>
-                            <img src={upakovka} alt="Упаковка" />
-                        </div>
-                    </Slider>
+                <h1 className='sites-title'>Несколько коммерческих проектов</h1>
+                <div className='site-grid'>
+                    <div className='site-grid-item'>
+                        <img src={avtoelectrika} alt='АвтоЭлектрика' />
+                        <a href='#!'>Автоэлектрика</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={bavlymuka} alt='Бавлы Мука' />
+                        <a href='#!'>Бавлы Мука</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={frbtlt} alt='Школа Карате' />
+                        <a href='#!'>Школа Карате</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={gippokrat16} alt='Медицинский центр Гиппократ' />
+                        <a href='#!'>Медицинский центр Гиппократ</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={kafolat} alt='Кафолат Центр' />
+                        <a href='#!'>Кафолат Центр</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={ngta} alt='НГТА' />
+                        <a href='#!'>НГТА</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={prodservis} alt='ПродСервис' />
+                        <a href='#!'>ПродСервис</a>
+                    </div>
+                    <div className='site-grid-item'>
+                        <img src={upakovka} alt='Упаковка32' />
+                        <a href='#!'>Упаковка32</a>
+                    </div>
                 </div>
             </div>
         </div>
