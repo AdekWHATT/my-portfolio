099cc108cd0b1aaf361ca7f6bf803a5b0b966ec5
Vyacheslaw Kilin <vyacheslaw.kilin@mail.ru>
2023-05-31 12:12:30 +0300

    3105git

--- src/components/Sites/Sites.js
+++ src/components/Sites/Sites.js
@@ -13,7 +13,7 @@ const Sites = () => {
     return (
         <div className='Sites'>
             <div className='container'>
-                <h1 className='sites-title'>1Несколько коммерческих проектов</h1>
+                <h1 className='sites-title'>Несколько коммерческих проектов</h1>
                 <div className='site-grid'>
                     <div className='site-grid-item'>
                         <img src={avtoelectrika} alt='АвтоЭлектрика' />
@@ -21,31 +21,31 @@ const Sites = () => {
                     </div>
                     <div className='site-grid-item'>
                         <img src={bavlymuka} alt='Бавлы Мука' />
-                        <a href='#!'>Бавлы Мука</a>
+                        <a href='https://bavly-muka.ru' target="_blank">Бавлы Мука</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={frbtlt} alt='Школа Карате' />
-                        <a href='#!'>Школа Карате</a>
+                        <a href='https://recordtlt.ru/' target="_blank">Школа Карате</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={gippokrat16} alt='Медицинский центр Гиппократ' />
-                        <a href='#!'>Медицинский центр Гиппократ</a>
+                        <a href='http://gippokrat16.ru' target="_blank">Медицинский центр Гиппократ</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={kafolat} alt='Кафолат Центр' />
-                        <a href='#!'>Кафолат Центр</a>
+                        <a href='https://kafolat-center.ru' target="_blank">Кафолат Центр</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={ngta} alt='НГТА' />
-                        <a href='#!'>НГТА</a>
+                        <a href='http://ngta.pro' target="_blank">НГТА</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={prodservis} alt='ПродСервис' />
-                        <a href='#!'>ПродСервис</a>
+                        <a href='https://prodservis-s.ru' target="_blank">ПродСервис</a>
                     </div>
                     <div className='site-grid-item'>
                         <img src={upakovka} alt='Упаковка32' />
-                        <a href='#!'>Упаковка32</a>
+                        <a href='https://упаковка32.рф' target="_blank">Упаковка32</a>
                     </div>
                 </div>
             </div>
